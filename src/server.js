e2af59b14eb3df245e13abcff8de34c3c8fa7b95
RISH136 <harsh03092003@gmail.com>
2025-09-25 13:12:08 +0530

    forth commit

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://speakeasy-frontend-1.onrender.com",
     credentials: true, // allow frontend to send cookies
   })
 );
